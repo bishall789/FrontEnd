f6d227d4bc0f40d46c674c63d4943fcaa217fb7d
Bishal Bista <bishalbista424@gmail.com>
2025-05-21 07:52:05 +0545

    Add files via upload

---
+++ 03_VARIABLES/script.js
@@ -0,0 +1,16 @@
+let name = 'Bishal'
+let age = 21
+
+let userIntro = 'My Name is ' + name + ' and I am ' + age + ' years old.'
+console.log(userIntro)
+
+const HourInDay = 24
+const DaysInWeek = 7
+let totalHoursInWeek = HourInDay * DaysInWeek
+console.log('Total Hours in a Week: ' + totalHoursInWeek)
+
+//HourInDay = 25 // This will throw an error because we cannot reassign a value to a constant variablecc
+console.log(ok)
+
+var ok= 'ok' // var is function scoped and can be redeclared 
+let
