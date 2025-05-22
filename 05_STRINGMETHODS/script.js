6901a7d4e4f2eab7ef1a3ff37c8d18673b1e96c0
Bishal Bista <bishalbista424@gmail.com>
2025-05-22 15:59:19 +0545

    Add files via upload

---
+++ 05_STRINGMETHODS/script.js
@@ -0,0 +1,14 @@
+let msg = " Bishal"
+let msg2 = "Hello, Anisha"
+console.log(msg)
+console.log(msg.length) 
+console.log(msg[1]) 
+console.log(msg.toUpperCase())
+console.log(msg.toLowerCase())
+let a = `Hi , My name is ${msg}`
+console.log(a)
+
+
+
+
+
