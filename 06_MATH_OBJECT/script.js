f2b97ff9ea179020c779b0ef98902b432ceba49d
Bishal Bista <bishalbista424@gmail.com>
2025-05-22 15:59:31 +0545

    Add files via upload

---
+++ 06_MATH_OBJECT/script.js
@@ -0,0 +1,5 @@
+// const height = +prompt("Enter your height : ");
+// const weidth = +prompt("Enter your weidth : ");
+// const area = height * weidth;
+// // alert('The area of rectange is : ' + area)
+// alert(`The area of rectangle is : ${area}`)
