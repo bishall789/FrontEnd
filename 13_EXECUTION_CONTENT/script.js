46a956b65d4ad194a242113dc810893558c06c2f
Bishal Bista <bishalbista424@gmail.com>
2025-05-29 08:44:50 +0545

    Add files via upload

---
+++ 13_EXECUTION_CONTENT/script.js
@@ -0,0 +1,25 @@
+// debugger
+
+firstfunction()
+secondfunction()
+thirdfunction()
+const name = "Bishal Bista"
+console.log(name)
+
+function firstfunction() {
+    const a = 2
+    const b = 3
+console.log("hi")
+}
+
+function secondfunction() {
+    const a = 2
+    const b = 3
+console.log("hi")
+}
+
+function thirdfunction() {
+    const a = 2
+    const b = 3
+console.log("hi")
+}
